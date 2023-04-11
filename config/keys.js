f8f2ae032b9166a7aed723468eaf2f7704bc51c7
Raunak Kumar <58897751+itsraunak@users.noreply.github.com>
2023-04-11 08:13:14 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: 'mongodb://localhost/habit_tracker'    
+    MongoURI: 'mongodb+srv://itisraunak:uouDJZpXHJhxTnWz@habittracker.c7uqs7p.mongodb.net/habit_tracker?retryWrites=true&w=majority'  
 }
